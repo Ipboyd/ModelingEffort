a9b1921e2033d2e2188cb783695f6af1729196a2
kfchou <kfchou@bu.edu>
2019-08-01 16:05:58 -0400

    semi successful recreation of fig 4

--- testMouseNetwork.m
+++ testMouseNetwork.m
@@ -12,6 +12,7 @@ y=1;
 x=1;
 ICdir = '104424';
 ICdirPath = ['import' filesep ICdir filesep];
+% ICdirPath = 'Z:\eng_research_hrc_binauralhearinglab\Model-Junzi_files_backup-remove_when_copied\V21\STRFs\163857\'
 ICstruc = dir([ICdirPath '*.mat']); % Having issues here where it is not actually recieving the imported data
 %fprintf("ICstruc length is %d\n",length(ICstruc));
 performanceMax=zeros(1,16);
@@ -20,8 +21,9 @@ pMaxs0=zeros(1,4);
 maxTaus0=zeros(1,4);
 maxTaum0=zeros(1,4);
 maxTau=zeros(1,16);
+plot_rasters = 0;
 
-for z = 14%1:length(ICstruc)
+for z = 1:length(ICstruc)
     load([ICdirPath ICstruc(z).name],'t_spiketimes');
     temp = cellfun(@max,t_spiketimes,'UniformOutput',false);
     tmax = max([temp{:}]);
@@ -39,9 +41,9 @@ for z = 14%1:length(ICstruc)
     % save spk file
 %     study_dir = fullfile(pwd, 'run', mfilename, filesep, num2str(z));
     study_dir = fullfile(pwd, 'run', num2str(z));
-    if ~exist(study_dir,'dir')
-        mkdir(fullfile(study_dir,'solve'));
-    end
+%     if ~exist(study_dir,'dir')
+%         mkdir(fullfile(study_dir,'solve'));
+%     end
 %     if exist(study_dir, 'dir')
 %       rmdir(study_dir, 's');
 %     end
@@ -52,7 +54,7 @@ for z = 14%1:length(ICstruc)
     save(fullfile(study_dir, 'solve','IC_spks.mat'),'spks');
 
     if ICstruc(z).name(2)=='0'
-        [performance, tau]= mouse_network(study_dir,size(spks,3));
+        [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
         % Take the max of the performance
         pMaxs0(z)=max(performance);
         maxTaus0(z)=tau(max(performance)==performance);
@@ -64,16 +66,16 @@ for z = 14%1:length(ICstruc)
             plotSpikeRasterFs(logical(squeeze(spks(:,ii,:))), 'PlotType','vertline');
             xlim([0 2000])
         end
-        [performance, tau]= mouse_network(study_dir,size(spks,3));
+        [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
         % Take the max of the performance
         pMaxm0(y)=max(performance);
         maxTaum0(y)=tau(max(performance)==performance);
         y= y+1;
     else
-        [performance, tau]= mouse_network(study_dir,size(spks,3));
+        [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
         % Take the max of the performance
         performanceMax(x)=max(performance);
-        maxTau(x)=tau(max(performance)==performance);
+        maxTau(x)=mean(tau(max(performance)==performance));
         x=x+1;
     end
 end
@@ -97,7 +99,7 @@ for i= 1:4
         t(i).Color= 'w';
     end
 end
-colormap('winter'); % this makes the boxes blue
+colormap('parula'); % this makes the boxes blue
 colorbar;
 caxis([50 100])
 %% output 1 by 4 grid of only target
@@ -117,12 +119,13 @@ for i= 1:4
         t(i).Color= 'w';
     end
 end
-colormap('winter'); % this makes the boxes blue
+colormap('parula'); % this makes the boxes blue
 colorbar;
 caxis([50 100])
 %% output- 4 by 4 grid- calculate in a seperate script
 % plot the matrix similar to the that on the paper- imagesc()
 mat= reshape(performanceMax,4,4);
+mat = flipud(mat);
 l=num2cell(mat);
 l= cellfun(@num2str, l,'UniformOutput', false);
 positionVector = [0.275 0.45 0.5 0.5];
@@ -139,9 +142,15 @@ for i= 1:16
         t(i).Color= 'w';
     end
 end
-colormap('winter'); % this makes the boxes blue
+colormap('parula'); % this makes the boxes blue
 colorbar;
 caxis([50 100])
+xlabel('song location')
+ylabel('masker location')
+yticks(1:4)
+yticklabels({'90','45','0','-90'})
+xticks(1:4)
+xticklabels({'-90','0','45','90'})
 
 %positionVector = [x0+subplotloc*(dx+lx) y0 lx ly];
 %subplot('Position',positionVector)
