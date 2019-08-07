4f72327f921caf9ab19d2180aaef8300e185bd7a
kfchou <kfchou@bu.edu>
2019-08-07 18:45:56 -0400

    selective trial testing

--- testMouseNetwork.m
+++ testMouseNetwork.m
@@ -10,19 +10,20 @@ addpath(genpath('../dynasim'))
 %% load IC spikes, convert cell array into binary array
 y=1;
 x=1;
-ICdir = '104424';
+ICdir = '151137';
 ICdirPath = ['import' filesep ICdir filesep];
 % ICdirPath = 'Z:\eng_research_hrc_binauralhearinglab\Model-Junzi_files_backup-remove_when_copied\V21\STRFs\163857\'
-ICstruc = dir([ICdirPath '*.mat']); % Having issues here where it is not actually recieving the imported data
-%fprintf("ICstruc length is %d\n",length(ICstruc));
+ICstruc = dir([ICdirPath '*.mat']);
+
+plot_rasters = 0;
+
 performanceMax=zeros(1,16);
 pMaxm0=zeros(1,4);
 pMaxs0=zeros(1,4);
 maxTaus0=zeros(1,4);
 maxTaum0=zeros(1,4);
 maxTau=zeros(1,16);
-plot_rasters = 0;
-
+diagConfigs = [6,12,18,24];
 for z = 1:length(ICstruc)
     load([ICdirPath ICstruc(z).name],'t_spiketimes');
     temp = cellfun(@max,t_spiketimes,'UniformOutput',false);
@@ -41,12 +42,6 @@ for z = 1:length(ICstruc)
     % save spk file
 %     study_dir = fullfile(pwd, 'run', mfilename, filesep, num2str(z));
     study_dir = fullfile(pwd, 'run', num2str(z));
-%     if ~exist(study_dir,'dir')
-%         mkdir(fullfile(study_dir,'solve'));
-%     end
-%     if exist(study_dir, 'dir')
-%       rmdir(study_dir, 's');
-%     end
     if exist(study_dir, 'dir')
       rmdir(study_dir, 's');
     end
@@ -57,19 +52,19 @@ for z = 1:length(ICstruc)
         [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
         % Take the max of the performance
         pMaxs0(z)=max(performance);
-        maxTaus0(z)=tau(max(performance)==performance);
+        maxTaus0(z)=mean(tau(max(performance)==performance));
 
     elseif ICstruc(z).name(4)=='0' %target only
-        figure;
-        for ii = 1:4
-            subplot(1,4,ii)
-            plotSpikeRasterFs(logical(squeeze(spks(:,ii,:))), 'PlotType','vertline');
-            xlim([0 2000])
-        end
+%         figure;
+%         for ii = 1:4
+%             subplot(1,4,ii)
+%             plotSpikeRasterFs(logical(squeeze(spks(:,ii,:))), 'PlotType','vertline');
+%             xlim([0 2000])
+%         end
         [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
         % Take the max of the performance
         pMaxm0(y)=max(performance);
-        maxTaum0(y)=tau(max(performance)==performance);
+        maxTaum0(y)=mean(tau(max(performance)==performance));
         y= y+1;
     else
         [performance, tau]= mouse_network(study_dir,size(spks,3),plot_rasters);
@@ -83,9 +78,10 @@ end
 
 %% output 1 by 4 grid of only masker
 figure
-l=num2cell(pMaxm0);
+textColorThresh = 70;
+l=num2cell(round(pMaxm0));
 l= cellfun(@num2str, l,'UniformOutput', false);
-positionVector = [0.1 0.25 0.85 0.1];
+positionVector = [0.35 0.8 0.35 0.1];
 subplot('Position',positionVector)
 im=imagesc(pMaxm0);
 x = [1,2,3,4];
@@ -93,19 +89,18 @@ y=[1,1,1,1];
 t= text(x(:),y(:), l, 'HorizontalAlignment', 'Center');
 for i= 1:4
     t(i).FontSize=12;
-    if pMaxm0(i)>85
+    if pMaxm0(i)>textColorThresh
         t(i).Color = 'k';
     else
         t(i).Color= 'w';
     end
 end
 colormap('parula'); % this makes the boxes blue
-colorbar;
 caxis([50 100])
-%% output 1 by 4 grid of only target
-l=num2cell(pMaxs0);
+% output 1 by 4 grid of only target
+l=num2cell(round(pMaxs0));
 l= cellfun(@num2str, l,'UniformOutput', false);
-positionVector = [0.1 0.1 0.85 0.1];
+positionVector = [0.35 0.65 0.35 0.1];
 subplot('Position',positionVector)
 im=imagesc(pMaxs0);
 x = [1,2,3,4];
@@ -113,22 +108,21 @@ y=[1,1,1,1];
 t= text(x(:),y(:), l, 'HorizontalAlignment', 'Center');
 for i= 1:4
     t(i).FontSize=12;
-    if pMaxs0(i)>85
+    if pMaxs0(i)>textColorThresh
         t(i).Color = 'k';
     else
         t(i).Color= 'w';
     end
 end
 colormap('parula'); % this makes the boxes blue
-colorbar;
 caxis([50 100])
-%% output- 4 by 4 grid- calculate in a seperate script
+% output- 4 by 4 grid- calculate in a seperate script
 % plot the matrix similar to the that on the paper- imagesc()
-mat= reshape(performanceMax,4,4);
+mat = reshape(performanceMax,4,4);
 mat = flipud(mat);
-l=num2cell(mat);
-l= cellfun(@num2str, l,'UniformOutput', false);
-positionVector = [0.275 0.45 0.5 0.5];
+l = num2cell(round(mat));
+l = cellfun(@num2str, l,'UniformOutput', false);
+positionVector = [0.35 0.15 0.4 0.4];
 subplot('Position',positionVector)
 im=imagesc(mat);
 x = repmat(1:4,4,1);
@@ -136,7 +130,7 @@ y=x';
 t= text(x(:),y(:), l, 'HorizontalAlignment', 'Center');
 for i= 1:16
     t(i).FontSize=12;
-    if mat(i)>85
+    if mat(i)>textColorThresh
         t(i).Color = 'k';
     else
         t(i).Color= 'w';
@@ -151,6 +145,6 @@ yticks(1:4)
 yticklabels({'90','45','0','-90'})
 xticks(1:4)
 xticklabels({'-90','0','45','90'})
-
+set(gca,'fontsize',12)
 %positionVector = [x0+subplotloc*(dx+lx) y0 lx ly];
 %subplot('Position',positionVector)
