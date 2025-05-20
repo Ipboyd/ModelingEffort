fd6d23cbd43c22d6b202c384d9df1001db7aed65
EBOWMAN26 <lcbowman@bu.edu>
2025-05-20 16:06:10 -0400

    color bar plus raster/hist plot

--- Multi-Channel/Plotting/OliverDataPlotting/Raster_Draft.m
+++ Multi-Channel/Plotting/OliverDataPlotting/Raster_Draft.m
@@ -14,6 +14,140 @@ load(['/Users/lbowman/Desktop/Research/sound_files.mat'],'sampleRate','target1',
 % load('all_units_info_with_polished_criteria_modified_perf.mat','all_data');
 % load('sound_files.mat','sampleRate','target1','target2');  %Sample Rate 195312 Hz
 
+%% plotting sound
+
+% 
+% [audioIn, fs] = audioread('200k_target1.wav');
+% vad = voiceActivityDetector()
+% 
+% isSpeech = vad(audioIn);
+% 
+% diffSpeech = diff([0; isSpeech; 0]);
+% onsets = find(diffSpeech == 1);
+% offsets = find(diffSpeech == -1) - 1;
+% 
+% % Convert to time (seconds)
+% 
+% onsetTimes = onsets / fs;
+% offsetTimes = offsets / fs;
+cd(userpath);
+cd('../GitHub/ModelingEffort/Single-Channel/Model/PreCortical-Modeling/resampled-stimuli')
+
+
+% Load target 1
+[x, fs] = audioread('200k_target1.wav');
+
+% Load target 2
+[x2, fs2] = audioread('200k_target1.wav');
+
+
+% Concatenate warm-up and original audio
+padded_audio = [x2; x];
+
+% Save to new file
+audiowrite('200k_target1_with_warmup.wav', padded_audio, fs);
+
+
+
+afr = dsp.AudioFileReader('200k_target1_with_warmup.wav');
+fs = afr.SampleRate;
+
+frameSize = ceil(5e-3*fs);
+overlapSize = ceil(0.8*frameSize);
+hopSize = frameSize - overlapSize;
+afr.SamplesPerFrame = hopSize;
+
+inputBuffer = dsp.AsyncBuffer('Capacity',frameSize);
+
+VAD = voiceActivityDetector('FFTLength',frameSize);
+
+scope = timescope('SampleRate',fs, ...
+    'TimeSpanSource','Property','TimeSpan',3, ...
+    'BufferLength',.5*fs, ...
+    'YLimits',[-1.5,1.5], ...
+    'TimeSpanOverrunAction','Scroll', ...
+    'ShowLegend',true, ...
+    'ChannelNames',{'Audio','Probability of speech presence'});
+
+player = audioDeviceWriter('SampleRate',fs);
+
+pHold = ones(hopSize,1);
+
+speechProbLog = []; 
+
+while ~isDone(afr)
+    x = afr();
+    n = write(inputBuffer,x);
+
+    overlappedInput = read(inputBuffer,frameSize,overlapSize);
+
+    p = VAD(overlappedInput);
+
+    pHold(end) = p;
+    scope(x,pHold)
+
+    player(x);
+
+    pHold(:) = p;
+
+    speechProbLog = [speechProbLog; p];
+end
+
+t = (0:length(speechProbLog)-1) * hopSize/fs;
+% plot(t, speechProbLog), xlabel('Time (s)'), ylabel('Speech Probability')
+
+count = 0;
+flag = 0;
+indexer = [];
+
+for k = 1:length(speechProbLog)
+
+    if speechProbLog(k) >= 0.95
+        count = count+1;
+        
+    end
+
+    if count == 35
+        indexer = [indexer,0];
+        indexer(end - 35 + 1 : end) = 1; %This should replace instead of add
+        flag = 1;
+        count = 0;
+    elseif flag == 1;
+        indexer = [indexer,1];
+        
+        if speechProbLog(k) < 0.95
+            flag = 0;
+        end
+       
+
+    else 
+        indexer = [indexer,0];
+        
+        
+
+    end
+
+end
+
+%%
+
+idx_nor = 10 .* indexer;
+ix = 4.6432e-04:4.6432e-04:3;
+
+op_idx = 10 - idx_nor;
+op_idx = abs(op_idx);
+
+figure;
+ 
+%plot(ix,idx_nor)
+area(ix,idx_nor,'FaceColor',[0.9882 0.7804 0.7804],'EdgeColor',[0.9882 0.7804 0.7804]); hold on
+area(ix,op_idx,'FaceColor',[0.7804 0.8392 0.9882],'EdgeColor',[0.7804 0.8392 0.9882]); hold off
+ 
+ylim([-0.5 10])
+
+
+%% plotting rasters & hists
+
 figure;                                         %Plot the target stimuli
 subplot(2,1,1)
 plot(target1)                                   %Stim  1
@@ -31,8 +165,14 @@ total_spike_data = [];
 
 for j = 1:length(labels)
 
+    total_spike_data = [];
+
     figure;
     subplot(3,1,[1 2])
+
+    area(ix,idx_nor,'FaceColor',[0.9882 0.7804 0.7804],'EdgeColor',[0.9882 0.7804 0.7804]); hold on
+    area(ix,op_idx,'FaceColor',[0.7804 0.8392 0.9882],'EdgeColor',[0.7804 0.8392 0.9882]);
+
 
     for k = 1:10
         
@@ -45,7 +185,7 @@ for j = 1:length(labels)
         y_lines(1,:) = k-1;
         y_lines(2,:) = k;
         y_lines(3,:) = nan;
-        h2 = plot(b,y_lines,'Color',[0 0 0 0.8],'LineWidth',0.4); hold on   %Plot
+        h2 = plot(b,y_lines,'Color',[0 0 0 0.8],'LineWidth',0.4);   %Plot
         total_spike_data = [total_spike_data; SpikeTimes];      %Save data for PSTH plotting
     
     end
@@ -53,9 +193,15 @@ for j = 1:length(labels)
     counts = histcounts(total_spike_data,'BinWidth',0.02); %Bin width of 20ms
     domain = linspace(-1,4,length(counts));
     
-    plot(domain,counts,'r','LineWidth',1.5)
+    cn_fac = 10 / max(counts);
+    counts_norm = counts .* cn_fac;
+
+    plot(domain,counts_norm,'r','LineWidth',1.5)
+    % area(ix,idx_nor,'FaceColor',[0.9882 0.7804 0.7804],'EdgeColor',[0.9882 0.7804 0.7804])
     
     hold off
+
+    firerate = length(total_spike_data)/5
 
     %Pad the first second at the start (Pre-onset)
     pre_onset = zeros(195312,1);
